e7109111594f402157b8b85ec935055f3e97f30c
Geoff Bourne <itzgeoff@gmail.com>
2022-10-23 14:56:30 -0500

    prune-branches: released master-SNAPSHOT

--- Formula/prune-branches.rb
+++ Formula/prune-branches.rb
@@ -1,5 +1,5 @@
 class PruneBranches < Formula
-  
+  desc ""
   homepage "https://github.com/itzg/prune-branches"
   url "https://github.com/itzg/test-github-releaser-plugin/releases/download/0.1.0/prune-branches-master-SNAPSHOT.tgz"
   sha256 "2f717a8e5b383a9f328268dfecb96c585deecd1cdf6cf7155d4b5de420d6a9ff"
