b35ccb5e3e46cdc5c6d89615c0a0408239448db8
Geoff Bourne <itzgeoff@gmail.com>
2022-11-13 17:19:31 -0600

    prune-branches: released master-SNAPSHOT

--- Formula/prune-branches.rb
+++ Formula/prune-branches.rb
@@ -1,8 +1,8 @@
 class PruneBranches < Formula
-  desc ""
+  desc "Prunes local git branches without remote branch"
   homepage "https://github.com/itzg/prune-branches"
   url "https://github.com/itzg/test-github-releaser-plugin/releases/download/0.1.0/prune-branches-master-SNAPSHOT.tgz"
-  sha256 "2f717a8e5b383a9f328268dfecb96c585deecd1cdf6cf7155d4b5de420d6a9ff"
+  sha256 "64b72659b216e61c5e69d6018298e63f740732ae4e136d887df3e412e9834178"
   license "MIT"
 
   depends_on "java"
